d1f5d6f0b2c9a3385936006b6d137f8329ac3311
Will <will_stutz1@baylor.edu>
2022-10-05 22:39:38 -0500

    initial commit

--- practiceClass.py
+++ practiceClass.py
@@ -12,3 +12,50 @@ change the status attribute to False.
 2) a Booking class that has attributes for customer name and
 seat number. Create accessor methods for both attributes.'''
         
+class Play:
+
+    def __init__(self,Id,name,numSeats,date):
+        self.__status = True
+        self.__id = Id
+        self.__name = name
+        self.__numSeats = numSeats
+        self.__date = date
+
+    def getName(self):
+        return self.__name
+
+    def getSeats(self):
+        return self.__numSeats
+
+    def getStatus(self):
+        return self.__status
+
+    def seatsLeft(self):
+        if self.__numSeats >= 0:
+            self.__numSeats = self.__numSeats - 1
+
+    def get_seatsLeft(self):
+        return self.__numSeats
+    
+    def set_status(self):
+        self.__status = False
+
+    def set_name(self, new_name):
+        self.__name = new_name
+    #danny method
+
+
+class Book:
+
+    def __init__(self,customerName,seatNumber):
+        self.__customerName = customerName
+        self.__seatNumber = seatNumber
+
+
+    def getName(self):
+        return self.__customerName
+    
+    def getSeat(self):
+        return self.__seatNumber
+
+    
--- practiceProgram.py
+++ practiceProgram.py
@@ -1,4 +1,4 @@
-import practiceClassKEY as p
+import practiceClass as p
 import csv
 
 
@@ -35,6 +35,22 @@ play class that has id 9587
 NOTE: Do not hard code the values to create the instance but use
 keys and values from the dictionary '''
 
+'''
+for record in shows:
+    if record == shows['play3']:
+        Id = shows['play3']['id']
+        name = shows['play3']['name']
+        capacity = shows['play3']['capacity']
+        event_date = shows['play3']['event_date']
+'''
+
+for record in shows:
+    if shows[record]['id'] == 9587:
+        the_play = p.Play(shows[record]['id'], shows[record]['name'],shows[record]['capacity'],shows[record]['event_date'])
+
+
+#print(the_play.getName())
+
 
 
 '''using the bookings.csv file process only those 
@@ -47,10 +63,23 @@ error message as shown in output.JPG'''
 
 #open the csv file in read mode
 
+infile = open('bookings.csv','r')
 
 #create a csv object from the file object from the step above
 
-
-
+csvfile = csv.reader(infile)
+next(csvfile)
 # use a for loop to iterate through each record in the bookings file
+
+for row in csvfile:
+    if row[0] == '9587':
+        the_customer = p.Book(row[1], row[2])
+        the_play.seatsLeft()
+
+        if the_play.get_seatsLeft() < 0:
+            print('******** ERROR ********')
+            print('Guest Name',the_customer.getName())
+            print('Sorry, Show:',the_play.getName(),'is sold out')
+            print('************************')
+            print()
 
